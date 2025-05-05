47d31f92f91a4a96fb723b228c51261b77eb61a4
tupai252 <adrianabad900@gmail.com>
2025-05-05 10:14:26 +0200

    Delete sayko24.github.io-main directory

--- sayko24.github.io-main/sayko24.github.io-main/script.js
+++
@@ -1,8 +0,0 @@
-const menuIcon = document.querySelector('.menu-icon');
-const menuList = document.querySelector('.menu-list');
-
-// Agregar un evento de clic al icono de la hamburguesa
-menuIcon.addEventListener('click', () => {
-    // Alternar la clase "show" para mostrar u ocultar el menú
-    menuList.classList.toggle('show');
-});
